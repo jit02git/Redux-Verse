99ae7200212e51b63deaa917a3b44d5158a2c2cc
jitesh <jitesh02sep@gmail.com>
2025-05-11 23:01:57 +0530

    renew the code

--- src/Redux/Hooks/index.js
+++ src/Redux/Hooks/index.js
@@ -1,4 +1,4 @@
-import { useDispatch, useSelector } from "react-redux";
-import type AppDispatch from "../store"
+// import { useDispatch, useSelector } from "react-redux";
+// import type AppDispatch from "../store"
 
-export const useAppDispatch: () => AppDispatch = useDispatch
+// export const useAppDispatch: () => AppDispatch = useDispatch
--- src/index.js
+++ src/index.js
@@ -1,22 +1,22 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "./index.css";
-import App from "./App";
-import reportWebVitals from "./reportWebVitals";
-import { Provider } from "react-redux";
-import Store from "../src/Redux/store"
+import React from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App'
+import { store } from './app/store'
+import { Provider } from 'react-redux'
 
+const container = document.getElementById('root')
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <React.StrictMode>
-    <Provider store={Store}>
+if (container) {
+  const root = createRoot(container)
+
+  root.render(
+    <Provider store={store}>
       <App />
-    </Provider>
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+    </Provider>,
+  )
+} else {
+  throw new Error(
+    "Root element with ID 'root' was not found in the document. Ensure there is a corresponding HTML element with the ID 'root' in your HTML file.",
+  )
+}
